f2816528be064be44f3235d108065ca749cee955
IMRAN HOSSEN <78139179+IMRANHOSSENME@users.noreply.github.com>
2023-12-09 00:26:22 +0600

    Add files via upload

---
+++ ত্রিভুজের ক্ষেত্রফল নির্ণয়ের.py
@@ -0,0 +1,10 @@
+def function(base, height):
+
+    area = 0.5 * base * height
+    return area
+
+length = float(input("ভুমির দৈর্ঘ্য ইনপুট দিন: "))
+height = float(input("উচ্চতা ইনপুট দিন: "))
+
+triangle_area = function(length, height)
+print(f"ত্রিভুজের ক্ষেত্রফল: {triangle_area}")
